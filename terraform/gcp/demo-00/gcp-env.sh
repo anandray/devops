836378e3f26266c3250b8ba784a101c8256dbbf4
anand ray <anand@wolk.com>
2020-01-18 19:23:32 -0800

    replaced hard coded values with vars

--- terraform/gcp/demo-00/gcp-env.sh
+++ terraform/gcp/demo-00/gcp-env.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-export TF_VAR_org_id=166764207190
-export TF_VAR_billing_account=01AAFE-340C42-AFE42E
-export TF_ADMIN=devops-112019
-export TF_CREDS=~/.gcloud/Terraform.json
+org_id=`gcloud organizations list --format text | grep organizations | cut -d "/" -f2`
+billing_account_id=`gcloud beta billing accounts list --format text | grep billingAccounts | cut -d "/" -f2`
+
+#Export the following variables to your environment for use throughout the tutorial.
+export TF_VAR_org_id=$org_id
+export TF_VAR_billing_account=$billing_account_id
+export TF_ADMIN=${USER}-terraform-admin
+export TF_CREDS=~/.config/gcloud/${USER}-terraform-admin.json
+
+#export TF_ADMIN=devops-112019
+#export TF_CREDS=~/.gcloud/Terraform.json
